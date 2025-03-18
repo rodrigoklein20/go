846b113f2843a6f02777b5866aadaaa89374ae2f
rodrigoklein20 <kleinrodrigo12@gmail.com>
2025-03-18 20:43:36 -0300

    curso go 18/03

---
+++ 21 - HTTP/http.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+
+	http.HandleFunc("/home", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Olá mundo"))
+	})
+
+	log.Fatal(http.ListenAndServe(":5000", nil))
+}
