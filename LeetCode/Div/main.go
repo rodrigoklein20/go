fb78c6e03881d5947e500e4ca863c58f868911f6
rodrigoklein20 <kleinrodrigo12@gmail.com>
2026-06-10 19:27:09 -0300

    Testando exercism

--- LeetCode/Div/main.go
+++ LeetCode/Div/main.go
@@ -6,14 +6,16 @@ import (
 )
 
 func main() {
-	log.Printf("RESULTADO: %v", shuffle([]int{2, 5, 1, 3, 4, 7}, 3))
+	// log.Printf("RESULTADO: %v", shuffle([]int{2, 5, 1, 3, 4, 7}, 3))
 
-	log.Printf("Máximo de números 1 consecutivos %d", findMaxConsecutiveOnes([]int{1, 1, 0, 1, 1, 1, 1, 0, 1, 1}))
+	// log.Printf("Máximo de números 1 consecutivos %d", findMaxConsecutiveOnes([]int{1, 1, 0, 1, 1, 1, 1, 0, 1, 1}))
 
-	findErrorNums([]int{1, 2, 2, 4, 5})
-	findErrorNums([]int{1, 1})
-	findErrorNums([]int{2, 2})
-	findErrorNums([]int{4, 2, 2, 1})
+	// findErrorNums([]int{1, 2, 2, 4, 5})
+	// findErrorNums([]int{1, 1})
+	// findErrorNums([]int{2, 2})
+	// findErrorNums([]int{4, 2, 2, 1})
+
+	smallerNumbersThanCurrent([]int{8, 2, 9, 3, 4})
 }
 
 func shuffle(nums []int, n int) []int {
@@ -80,4 +82,21 @@ func findErrorNums(nums []int) []int {
 
 	log.Print("Saída", ret)
 	return ret
+}
+
+func smallerNumbersThanCurrent(nums []int) []int {
+	var maior int
+	var ocurr int
+
+	for _, n := range nums {
+		if n > maior {
+			maior = n
+			ocurr = 1
+		} else if n == maior {
+			ocurr++
+		}
+	}
+	log.Printf("%d menores", len(nums)-ocurr)
+
+	return []int{}
 }
